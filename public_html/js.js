e4918064c3e618990e8fefba047005ac143b7580
Bíborka <balogh01biborka@gmail.com>
2021-03-17 10:58:50 +0100

    5-el osztásBB

--- public_html/js.js
+++ public_html/js.js
@@ -9,7 +9,7 @@ function feltolt(){
 //    tomb[1]="macska";
 //    tomb[2]=False;
     for (var i = 0; i < nev; i++) {
-        var vel=Math.floor(Math.random()*10);//[0;1[ -> [0;10[ //Math.floor lefelé kerekít, levágja a 0.1-es jegyeket
+        var vel=Math.floor(Math.random()*10+1);//[0;1[ -> [0;10[ //Math.floor lefelé kerekít, levágja a 0.1-es jegyeket
         tomb[i]=vel;
     }
 }
@@ -29,6 +29,7 @@ function dolgozz(){
 //    console.log("Hello") //a vizsgálatnál a consolhoz ir hozzá
 
     feltolt();
+    osztas();
     console.log(tomb);//kiiratjuk a consolba az elemeket
     document.getElementById("eredmeny").innerHTML="Az eredmény: <br> "+ tomb.join("; ");//.join(; ) vesszovel elválasztva írja ki   
     osszegzes();
@@ -36,11 +37,21 @@ function dolgozz(){
 }
 
 function init(){
-    console.log("Első üzenet")
+    console.log("Első üzenet");
     document.getElementById("eredmeny").innerHTML+=" Én és a JS már dolgozik";//nem jó, mert még nincs ilyen elem, amikor mi mar bele akarunk iratni
-    document.getElementById("gomb").addEventListener("click", dolgozz) //az index.htmlben az onclick="kiir()" helyett
+    document.getElementById("gomb").addEventListener("click", dolgozz); //az index.htmlben az onclick="kiir()" helyett
+}
+
+function osztas() {
+    var db = 0;
+    for (var i = 0; i < tomb.length; i++) {
+        if(tomb[i]%5===0){
+            db++;
+        }
+    }
+    document.getElementById("osztas").innerHTML="5-el osztható számok:"+db + "db";
 }
 
 
-window.addEventListener("load", init)//ezzel kell mi9ndig kezdeni!! ha lefut a program, akkor utána mehet a többi
+window.addEventListener("load", init);//ezzel kell mi9ndig kezdeni!! ha lefut a program, akkor utána mehet a többi
 
